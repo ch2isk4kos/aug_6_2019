0b9b352e4ce61724c91ee5954ea21d9aea655b18
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-09 20:03:36 -0400

    added crud actions to controllers

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,30 @@
 class PlayersController < ApplicationController
+
+    before_action :find_player, only: [:show]
+
+    def index
+        @players = Player.all
+    end
+
+    def new
+        @player = Player.new
+    end
+
+    def create
+        @player = Player.new(player_params)
+    end
+
+    def show
+    end
+
+    private
+
+    def player_params
+        params.require(:player).permit(:name, :position, :number)
+    end
+
+    def find_player
+        @player = Player.find(params[:id])
+    end
+    
 end
--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,2 +1,31 @@
 class RankingsController < ApplicationController
+
+    before_action :find_ranking, only: [:show]
+
+    def index
+        @rankings = Ranking.all
+    end
+
+    def new
+        @ranking = Ranking.new
+    end
+
+    def create
+        @ranking = Ranking.new(ranking_params)
+    end
+
+    def show; end
+
+    private
+
+    def ranking_params
+        params.require(:ranking).permit(:description, selections_attributes: [:ranking_position])
+    end
+
+    #, player_ids: [], players_attributes: [:name, :position, :number]
+
+    def find_ranking
+        @ranking = Ranking.find(params[:id])
+    end
+    
 end
