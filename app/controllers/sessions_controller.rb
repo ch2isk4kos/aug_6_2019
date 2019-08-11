f1d87b787e8ad1d467e4694365b50a6b690028a3
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-11 18:16:21 -0400

    generated sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+    def new; end
+
+    # login
+    def create
+        user = User.find_by(email: params[:user][:email])
+        
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user
+        else
+            render :new
+        end
+    end
+
+    # logout
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
+end
