09813fba6ad847f484572abdba4c0ab5b70ba8b7
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-11 17:50:39 -0400

    associated models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :ranking
+    belongs_to :user
+    belongs_to :ranking
+
 end
--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,4 +1,6 @@
 class Ranking < ApplicationRecord
+    has_many :comments
+    has_many :users, through: :comments
     has_many :selections
     has_many :players, through: :selections
 
